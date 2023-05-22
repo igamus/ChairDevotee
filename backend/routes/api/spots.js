91c8281026dbb96a14973547edf66674b32d4c4f
Isaac Gamus <igamus@gmail.com>
2023-05-21 22:44:23 -0400

    Bug fix attempt 2 for prod `GET /spots/:spotId` endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -373,7 +373,7 @@ router.get('/:spotId', async (req, res) => {
                 [sequelize.fn('AVG', sequelize.col('Reviews.stars')), 'avgStarRating']
             ],
         },
-        group: ['Spot.id'],
+        group: ['Spot.id', 'SpotImages.id'],
         include: [
             {
                 model: Review,
