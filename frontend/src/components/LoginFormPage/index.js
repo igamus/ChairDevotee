ff8d8d08a86bff9a3cd4e30ad5d59f0e072d3346
Isaac Gamus <igamus@gmail.com>
2023-06-05 00:50:33 -0400

    Add rudimentary login

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,64 @@
+/* TODO:
+    - input validation
+    - prevent button press when errors exist
+    - add display for that
+    - css
+*/
+
+import { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import './LoginForm.css';
+
+function LoginFormPage() {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState('');
+
+    console.log('sessionUser:', sessionUser);
+    if (sessionUser) return <Redirect to='/' />;
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setErrors({});
+        return dispatch(sessionActions.login({ credential, password })).catch(
+            async res => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            }
+        );
+    };
+
+    return (
+        <>
+            <h1>Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username or Email
+                    <input
+                        type='text'
+                        value={credential}
+                        onChange={e => setCredential(e.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        required
+                    />
+                </label>
+                {errors.credential && <p>{errors.credential}</p>}
+                <button type='submit'>Log In</button>
+            </form>
+        </>
+    )
+}
+
+export default LoginFormPage;
